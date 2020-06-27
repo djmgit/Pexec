f7badecfcbe594a02ac7e2759a83d6279e6c62d2
Deepjyoti Mondal <djmdeveloper060796@gmail.com>
2020-06-27 20:30:46 +0530

    Add commandline options

--- cmd/pexec.go
+++ cmd/pexec.go
@@ -15,6 +15,11 @@ func main() {
 	tagValue := flag.String("tag_value", "", "tag value")
 	servers := flag.String("servers", "", "Server ip and port in format <IP>:<PORT>, multuple values can be separated by ','. If port is not provided then 22 will be used as default SSH port")
 	port := flag.Int("port", 0, "Port to override for all")
+	provider := flag.String("provider", "", "Provider of servers - can be one of CUSTOM | AWS")
+	parallel := flag.Bool("parallel", true, "If true then commands will be exected in parallel on the discovered or provided servers")
+	batchSize := flag.Int("batch_size", 0, "If more than one, then batches of that many servers will be executed in parallel")
+	user := flag.String("user", "", "User which will be used to login to the server")
+	keyPath := flag.String("user", "", "If not provided then default key path for rsa key will be used - /home/<user>/.ssh/id_rsa")
 }
 
 
