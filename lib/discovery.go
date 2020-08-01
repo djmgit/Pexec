e3e855cbdd69fdbcfd1fa862c36ccf602bf200a3
Deepjyoti Mondal <djmdeveloper060796@gmail.com>
2020-08-01 19:58:12 +0530

    Add inline documentation

--- lib/discovery.go
+++ lib/discovery.go
@@ -4,10 +4,13 @@ import (
 	"log"
 )
 
+// Provider interface to hold the desired interface
 type Provider interface {
 	GetServers(map[string]string, *log.Logger) ([]string, error)
 }
 
+// Fucntion to return the prober provider handler using the above interface
+// As of now it only supports AWS
 func GetProviderHandler(providerType string, logger *log.Logger) (Provider) {
 
 	var provider Provider
