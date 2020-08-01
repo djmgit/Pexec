77b4004cc5a28c6f58fc1ff12bc93b3984d035b4
Deepjyoti Mondal <djmdeveloper060796@gmail.com>
2020-08-01 20:01:16 +0530

    Add inline documentation

--- lib/providers.go
+++ lib/providers.go
@@ -9,18 +9,22 @@ import (
 
 type AWSProvider struct {}
 
+// Fucntion for discovering AWS instance IPs
 func (provider AWSProvider) GetServers(providerOptions map[string]string, logger *log.Logger) ([]string, error) {
-	
+
+	// Initialise Discover struct from go-discover
 	discoverer := discover.Discover{
 		Providers : map[string]discover.Provider{
 			"aws": discover.Providers["aws"],
 		},
 	}
 
+	// Discard logs if loggger is not set
 	if logger == nil {
 		logger = log.New(ioutil.Discard, "", log.LstdFlags)
 	}
 
+	// Create the constraint list for discovering AWS instances
 	cfg := fmt.Sprintf("provider=aws region=%s access_key_id=%s secret_access_key=%s addr_type=%s tag_key=%s tag_value=%s", providerOptions["region"], providerOptions["accessKeyId"], providerOptions["secretAccessKey"], providerOptions["addrType"], providerOptions["tagKey"], providerOptions["tagValue"])
 	serverIps, err := discoverer.Addrs(cfg, logger)
 
